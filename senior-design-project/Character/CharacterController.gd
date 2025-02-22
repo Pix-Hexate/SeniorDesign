37266aface6fe462f5b5bfd02a6949a0d63ec156
BZ <99069965+B-Zhen@users.noreply.github.com>
2025-02-22 14:43:03 -0500

    Input buffer implemented, starting basic animations

--- senior-design-project/Character/CharacterController.gd
+++ senior-design-project/Character/CharacterController.gd
@@ -1,11 +1,14 @@
-class_name KnightCharacterClass extends CharacterBody2D
+class_name CharacterBaseScene extends CharacterBody2D
 
 '''Movement Stuff'''
 var MovementVector : float = 0
 var MoveSpeed : float = 500
-var JumpStrength : float = 1000
-var GravityStrength : float = 3000
-@onready var Sprite : AnimatedSprite2D = $AnimatedSprite2D
+var JumpStrength : float = 1500
+var GravityStrength : float = 6000
+@onready var AnimPlayer : AnimationPlayer = $AnimationPlayer
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,22 +16,70 @@ func _ready() -> void:
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	MovementVector = 0
-	if Input.is_action_pressed("LeftKey"): #we take the input keys to make a left/right "vector", and later on multiply the vector by movespeed to actually move
-		MovementVector -= 1
+	Take_Inputs()
+	Process_Movement_Inputs()
+
+var Buffered_Keys : Dictionary = {"Movement" : "", "Action" : ""}
+@export var Playing_Action : bool = false
+@onready var BufferTimer : Timer = $InputBufferTimer
+#There will be two key - value pairings
+#movement, and "action"
+func Take_Inputs(): #This function is our input buffer
+	'''Movement'''
+	if Input.is_action_pressed("LeftKey"): 
+		Buffered_Keys["Movement"] = "LeftKey"
 	if Input.is_action_pressed("RightKey"):
-		MovementVector += 1
+		if Buffered_Keys["Movement"] == "LeftKey":
+			Buffered_Keys["Movement"] = ""
+		else:
+			Buffered_Keys["Movement"] = "RightKey"
 	if Input.is_action_just_pressed("UpKey"): 
-		AttemptJump()
-	SetAnimation()
+		Buffered_Keys["Action"] = "Jump"
+		BufferTimer.start()
+		
 	
+	if Input.is_action_pressed("AbilityOneKey"):
+		Buffered_Keys["Action"] = "AbilityOne"
+		BufferTimer.start()
+	if Input.is_action_pressed("AbilityTwoKey"):
+		Buffered_Keys["Action"] = "AbilityTwo"
+		BufferTimer.start()
+	if Input.is_action_pressed("AbilityThreeKey"):
+		Buffered_Keys["Action"] = "AbilityThree"
+		BufferTimer.start()
+	if Input.is_action_pressed("AbilityFourKey"):
+		Buffered_Keys["Action"] = "AbilityFour"
+		BufferTimer.start()
+
+
+func Process_Movement_Inputs():
+	MovementVector = 0
+	match Buffered_Keys["Movement"]:
+		"LeftKey":
+			MovementVector = -1
+			Buffered_Keys["Movement"] = ""
+		"RightKey":
+			MovementVector = 1
+			Buffered_Keys["Movement"] = ""
+		
+	if Buffered_Keys["Action"] == "Jump":
+		AttemptJump()
+		
+	SetAnimation()
+
 func AttemptJump() -> void:
 	var canjump = true #to add canjump logic later #TODO
 	if canjump and is_on_floor():
 		velocity.y -= JumpStrength
+		Buffered_Keys["Action"] = ""
 	
 func _physics_process(delta: float) -> void: 
+	Process_Movement_Physics(delta)
+	
+
+func Process_Movement_Physics(delta : float):
 	velocity.y += GravityStrength*delta
+	velocity.y = clampf(velocity.y, -9999, 1000) #clamp downwards fall velocity
 	if MovementVector:
 		velocity.x = MovementVector*MoveSpeed
 	
@@ -37,18 +88,20 @@ func _physics_process(delta: float) -> void:
 		velocity.x = 0
 		
 	move_and_slide()
-	
-# I'm considering whether or not we'll need a state machine for our animations, or whether or not we can just use base code logic, I think we can get away with no FSM?
+	print(velocity.y)
 
 #First, check if going up or down, those always come first
 #Then, check if walking, if not walking or up or down, then idle
 func SetAnimation() -> void:
 	if ((not is_on_floor()) and (velocity.y >= 0)):
-		print("playing jumpdown")
-		Sprite.play("jumpdown")
+		AnimPlayer.play("JumpDown")
 	elif velocity.y < 0:
-		Sprite.play("jumpup")
+		AnimPlayer.play("JumpUp")
 	elif velocity.x != 0:
-		Sprite.play("walk")
+		AnimPlayer.play("Walk")
 	else:
-		Sprite.play("idle")
+		AnimPlayer.play("Idle")
+
+
+func BufferTimeout():
+	Buffered_Keys["Action"] = ""
