aefed8f898f621c935cd8d817c69f5425cfcafde
Calamiturge <99069965+B-Zhen@users.noreply.github.com>
2025-03-16 17:47:43 -0400

    enemy example done - basic wip

--- senior-design-project/Entities/Character/CharacterController.gd
+++ senior-design-project/Entities/Character/CharacterController.gd
@@ -3,7 +3,7 @@ class_name CharacterBaseScene extends CharacterBody2D
 '''Movement Stuff'''
 var MovementVector : float = 0
 var MoveSpeed : float = 250
-var JumpStrength : float = 250
+var JumpStrength : float = 600 #250 default
 var GravityStrength : float = 1200
 @onready var AnimPlayer : AnimationPlayer = $AnimationPlayer
 
--- senior-design-project/Entities/Enemy/Basic Walker/Walker_Enemy.gd
+++ senior-design-project/Entities/Enemy/Basic Walker/Walker_Enemy.gd
@@ -9,10 +9,27 @@ extends EnemyBaseScene
 3 - Wandering
 4 - Wandering, but player within detection area
 
-3 <-> 4
-3,4 -> 1
-1 -> 2,3,4
-2 -> 1 
+"base phase" is 3
+When player enters detection radius, cast a raycast to see if the player is actually "visible" to the enemy
+if they are, then enter 1
+
+1 - Seeking
+If player leaves, start timer, to enter 3
+If player reenters, stop timer
+If Player hits them, restart timer if they are outside, or stop if inside
+Basically, the enemy only stops seeking after the player is outside their detection
+radius for x seconds and dpe
+
+2 - Got hit, and stunned for a brief period
+immediately enter 1 after stun regardless distance
+
+3 - Wandering
+Basic behavior if player is outside of radius
+
+4 - Wandering*
+Player went inside detection, but not "Visible" due to collision blocking vision
+In this state, start a timer to create a raycast to attempt to "re-detect" player every 1s
+If player leaves, go back to 3
 '''
 var AI_Phase : int = 3
 var Want_Right : bool = true #Which way the enemy "wants" to go, and determines the way its facing
@@ -21,16 +38,22 @@ var Acceleration : float = 500 #takes slightly over .5s to get to max speed
 var Max_Speed : float = 300 #Player is 250 for reference
 var JumpStrength : float = 250 #Player is 250
 var GravityStrength : float = 1200 #Player is 1200
+@onready var StunTimer : Timer = $StunTimer
+@onready var PlayerAggroTimer : Timer = $PlayerAggroTimer
+@onready var WallDetector : RayCast2D = $"Wall Detector"
 func _AI():#Override This
 	match AI_Phase:
 		1:
-			pass
+			$TEST_PHASE_INDICATOR.text = "1"
+			
 		2:
-			pass
+			$TEST_PHASE_INDICATOR.text = "2"
 		3:
-			pass
+			$TEST_PHASE_INDICATOR.text = "3"
+		4:
+			$TEST_PHASE_INDICATOR.text = "4"
 		_:
-			print("ERROR IN ENEMY AI - AI is not 1-3? How is that even possible????")
+			print("ERROR IN WALKER ENEMY AI - AI is not 1-4? How is that even possible????")
 
 
 
@@ -39,9 +62,21 @@ func _physics_process(delta): #Override this
 		velocity += get_gravity() * delta
 	move_and_slide()
 
+	WallDetector.global_position = Vector2(global_position.x, PlayerRef.global_position.y)
+	WallDetector.target_position = to_local(PlayerRef.global_position) - WallDetector.position
+	WallDetector.force_raycast_update()
+	if WallDetector.get_collider() is CharacterBaseScene:
+		print("seeing player")
+	else:
+		print("not seeing player")
+
 
 func Got_Hit(Data : AttackData): #Override This
-	print("i got hit")
+	PlayerAggroTimer.stop()
+	AI_Phase = 2
+	velocity.x = 0
+	#TODO Knockback
+	
 	$"Delete this - testing only".visible = true
 	$"Delete this - testing only/Timer".start()
 
@@ -49,10 +84,17 @@ func delete_this_testing_only():
 	$"Delete this - testing only".visible = false
 
 
-func PlayerDetection(body: Node2D) -> void: #override this
-	if body is CharacterBaseScene: #need a raycast to make sure there's no terrain in the way
-		AI_Phase = 1
-		$PlayerAggroTimer.stop()
+func PlayerDetection(body: Node2D = null) -> void: #this ever only detects players anyways
+	if PlayerRef.global_position.y > global_position.y: #if player is under us, we always see him
+		print("See Player")
+	else: #if the player is over us, draw a line to them to see if we can see
+		WallDetector.global_position = Vector2(global_position.x, PlayerRef.global_position.y)
+		WallDetector.target_position = to_local(PlayerRef.global_position) - WallDetector.position
+		WallDetector.force_raycast_update()
+		if WallDetector.get_collider() is CharacterBaseScene:
+			print("seeing player")
+		else:
+			print("not seeing player")
 		
 func PlayerDetection_Leave(body: Node2D) -> void:
 	if body is CharacterBaseScene:
@@ -60,3 +102,6 @@ func PlayerDetection_Leave(body: Node2D) -> void:
 
 func DropAggro() -> void:
 	AI_Phase = 3
+
+func EndStun() -> void:
+	AI_Phase = 1
