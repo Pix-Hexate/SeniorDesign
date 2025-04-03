b0d8356cba5ceca1a4b3c52ed5923956a6be6393
BZ <bailin.zhen@gmail.com>
2025-04-02 22:51:11 -0400

    Item Pickup (STILL NEED STATS FOR APPLYUPGRADE)

--- senior-design-project/Entities/Items/Item Resoures/Item_Data.gd
+++ senior-design-project/Entities/Items/Item Resoures/Item_Data.gd
@@ -1,6 +1,6 @@
 class_name Item_Data extends Node
 
 #README TODO There will probably be more variables in here in the future, this is just very barebones for example
-var Name : String
-var Effects : Dictionary
+var Name : String = "DEFAULT"
+var Effects : Dictionary = {"DEFAULT":"DEFAULT"}
 var Icon : Texture2D
--- senior-design-project/Entities/Items/item_object.gd
+++ senior-design-project/Entities/Items/item_object.gd
@@ -1,9 +1,10 @@
 class_name Item_Object extends Node2D
-var Data : Item_Data
+var Data : Item_Data 
 @onready var Icon : TextureRect = $TextureRect
 @onready var Text : RichTextLabel = $Panel/RichTextLabel
-
-
+@onready var Display : Panel = $Panel
+@onready var PlayerRef : CharacterBaseScene = get_tree().get_first_node_in_group("Player")
+var Taken : bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if is_instance_valid(Data):
@@ -15,16 +16,25 @@ func _ready():
 			Text.text += " : "
 			Text.text += str(Data.Effects[key]) #TODO NEED A SPECIAL EFFECTS READER
 			Text.text += "\n"
+	else:
+		Data = Item_Data.new()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Display.visible:
+		if Input.is_action_pressed("InteractKey"): 
+			Take_Item()
 
+func Take_Item():
+	if not Taken:
+		Taken = true
+		PlayerRef.ApplyUpgrade(Data)
+		queue_free()
 
 
 func Player_Entered(body):
-	$Panel.visible = true
+	Display.visible = true
 
 
 func Player_Left(body):
-	$Panel.visible = false
+	Display.visible = false
